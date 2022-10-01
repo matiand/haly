9eb9e8a5bf14048168a5abc68762d07690fe58dc
matiand <15248621+matiand@users.noreply.github.com>
2022-10-01 18:47:18 +0200

    Explain why we need to set 'userMarket' explicitly

--- Haly.WebApp/ThirdPartyApis/Spotify/ISpotifyService.cs
+++ Haly.WebApp/ThirdPartyApis/Spotify/ISpotifyService.cs
@@ -8,6 +8,11 @@ public interface ISpotifyService
 {
     Task<User> GetCurrentUser();
     Task<List<Playlist>> GetCurrentUserPlaylists();
+
+    // Spotify documentation states that when you omit 'userMarket', the market
+    // associated with user account that requests data will be used (based on
+    // access token). From testing this seems not true, so when you need data
+    // for your market, set 'userMarket' explicitly.
     Task<List<Track>> GetPlaylistTracks(string playlistId, string userMarket);
     Task<List<Track>> GetLikedSongs(string userMarket);
     Task<List<DeviceDto>> GetAvailableDevices();
