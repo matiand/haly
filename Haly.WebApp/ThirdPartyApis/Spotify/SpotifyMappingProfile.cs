e3e92dd159aec4f884cfb8cf186bbb9eee0064e8
matiand <15248621+matiand@users.noreply.github.com>
2022-10-17 14:50:34 +0200

    Fix broken mapping of SpotifyId in Tracks

--- Haly.WebApp/ThirdPartyApis/Spotify/SpotifyMappingProfile.cs
+++ Haly.WebApp/ThirdPartyApis/Spotify/SpotifyMappingProfile.cs
@@ -20,7 +20,7 @@ public class SpotifyMappingProfile : IRegister
             .Map(dest => dest.Owner.Name, src => src.Owner.Display_name);
 
         config.ForType<PlaylistTrackObject, Track>()
-            .Map(dest => dest.SpotifyId, src => src)
+            .Map(dest => dest.SpotifyId, src => src.Track.Id)
             .Map(dest => dest.Name, src => src.Track.Name)
             .Map(dest => dest.Album, src => src.Track.Album)
             .Map(dest => dest.Artists, src => src.Track.Artists)
