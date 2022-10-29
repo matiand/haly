be1b523a8c5338af6b7b9770cbc4c143166e16a0
matiand <15248621+matiand@users.noreply.github.com>
2022-10-29 13:03:16 +0200

    Allow Tracks to have nullable SpotifyId
    Tracks from user's Local Library have that set to null.

--- Haly.WebApp/Migrations/20221029104335_AllowNullableSpotifyIdForTracks.Designer.cs
+++ Haly.WebApp/Migrations/20221029104335_AllowNullableSpotifyIdForTracks.Designer.cs
@@ -5,6 +5,7 @@ using Haly.WebApp.Data;
 using Haly.WebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Haly.WebApp.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20221029104335_AllowNullableSpotifyIdForTracks")]
+    partial class AllowNullableSpotifyIdForTracks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Haly.WebApp/Migrations/20221029104335_AllowNullableSpotifyIdForTracks.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Haly.WebApp.Migrations
+{
+    public partial class AllowNullableSpotifyIdForTracks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SpotifyId",
+                table: "Tracks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SpotifyId",
+                table: "Tracks",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
