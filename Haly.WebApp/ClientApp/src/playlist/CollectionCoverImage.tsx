09b6717515847600b66b6c6ffc6bf49f72aa8d65
matiand <15248621+matiand@users.noreply.github.com>
2023-02-11 18:30:16 +0100

    Extract color from top left album cover when handling mosaic images
    It seems that's how Spotify does it. Current version is only temporary,
    we build a canvas for cropping the mosaic, then we extract color from
    it. It's slow, so we will have to write our own extraction library that
    allows for cropping before getting the colors.

--- Haly.WebApp/ClientApp/src/playlist/CollectionCoverImage.tsx
+++ Haly.WebApp/ClientApp/src/playlist/CollectionCoverImage.tsx
@@ -3,11 +3,8 @@ import ColorThief from "color-thief-ts";
 import { useSetAtom } from "jotai";
 import { useCallback, useRef } from "react";
 
-import { AlbumDto } from "../../generated/haly";
 import { collectionDominantColorAtom } from "../common/atoms";
-import EmptyCoverImage from "../common/EmptyCoverImage";
-import { styled, theme } from "../common/theme";
-import TrackCoverImage from "../common/TrackCoverImage";
+import { styled } from "../common/theme";
 
 type CoverImageProps = {
     alt: string;
@@ -17,11 +14,27 @@ type CoverImageProps = {
 function CollectionCoverImage({ alt, imageUrl }: CoverImageProps) {
     const setCollectionDominantColor = useSetAtom(collectionDominantColorAtom);
     const imageRef = useRef<HTMLImageElement>(null);
+    const secondImageRef = useRef<HTMLImageElement>(null);
 
     const onImageLoad = useCallback(() => {
         console.time("Dominant color extraction");
+
+        const canvas = document.createElement("canvas");
+        canvas.height = 320;
+        canvas.width = 320;
+        canvas.getContext("2d")!.drawImage(imageRef.current!, 0, 0, 320, 320, 0, 0, 320, 320);
+        secondImageRef.current!.src = canvas.toDataURL("image/jpeg");
+    }, []);
+
+    const extractColor = useCallback(() => {
         const thief = new ColorThief();
-        const dominantColor = thief.getColor(imageRef.current!, { colorType: "hex" });
+
+        let dominantColor;
+        if (imageUrl.includes("//mosaic")) {
+            dominantColor = thief.getColor(secondImageRef.current!, { colorType: "hex" });
+        } else {
+            dominantColor = thief.getColor(imageRef.current!, { colorType: "hex" });
+        }
 
         let ratio = chroma.contrast("white", dominantColor);
         let finalColor = dominantColor;
@@ -40,15 +53,30 @@ function CollectionCoverImage({ alt, imageUrl }: CoverImageProps) {
         console.log("Final color + ratio", finalColor, ratio);
         setCollectionDominantColor(finalColor);
         console.timeEnd("Dominant color extraction");
-    }, [setCollectionDominantColor]);
+    }, [imageUrl, setCollectionDominantColor]);
 
     return (
-        <Image alt={alt} src={imageUrl} loading="eager" crossOrigin="anonymous" ref={imageRef} onLoad={onImageLoad} />
+        <>
+            <Image
+                alt={alt}
+                src={imageUrl}
+                loading="eager"
+                crossOrigin="anonymous"
+                ref={imageRef}
+                onLoad={onImageLoad}
+            />
+            <img
+                alt="hidden img for color extraction"
+                ref={secondImageRef}
+                onLoad={extractColor}
+                style={{ display: "none" }}
+            />
+        </>
     );
 }
 
 const Image = styled("img", {
-    $$size: "200px",
+    $$size: "210px",
 
     height: "$$size",
     width: "$$size",
