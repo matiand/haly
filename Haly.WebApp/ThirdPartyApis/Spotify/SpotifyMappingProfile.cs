b744f868889e3af70095bd30a9a497cfe51c6340
matiand <15248621+matiand@users.noreply.github.com>
2023-06-09 23:21:32 +0200

    Optimize image selection for playlists and tracks
    The dimensions of our displayed images are smaller than what used to
    fetch, now we choose smaller images.

--- Haly.WebApp/ThirdPartyApis/Spotify/SpotifyMappingProfile.cs
+++ Haly.WebApp/ThirdPartyApis/Spotify/SpotifyMappingProfile.cs
@@ -19,14 +19,14 @@ public class SpotifyMappingProfile : IRegister
 
         config.ForType<SimplifiedPlaylistObject, Playlist>()
             .Map(dest => dest.SnapshotId, src => src.Snapshot_id)
-            .Map(dest => dest.Metadata.ImageUrl, src => src.Images.Any() ? src.Images.First().Url : null)
+            .Map(dest => dest.Metadata.ImageUrl, src => GetPlaylistImage(src.Images))
             .Map(dest => dest.Metadata.Description, src => TrimAndDecodePlaylistDescription(src.Description))
             .Map(dest => dest.Metadata.Owner.Id, src => src.Owner.Id)
             .Map(dest => dest.Metadata.Owner.Name, src => src.Owner.Display_name);
 
         config.ForType<PlaylistObject, Playlist>()
             .Map(dest => dest.SnapshotId, src => src.Snapshot_id)
-            .Map(dest => dest.Metadata.ImageUrl, src => src.Images.Any() ? src.Images.First().Url : null)
+            .Map(dest => dest.Metadata.ImageUrl, src => GetPlaylistImage(src.Images))
             .Map(dest => dest.Metadata.Description, src => TrimAndDecodePlaylistDescription(src.Description))
             .Map(dest => dest.Metadata.Owner.Id, src => src.Owner.Id)
             .Map(dest => dest.Metadata.Owner.Name, src => src.Owner.Display_name)
@@ -53,25 +53,34 @@ public class SpotifyMappingProfile : IRegister
                 src => src.Track.Type == TrackObjectType.Track ? TrackType.Song : TrackType.Podcast);
 
         config.ForType<SimplifiedAlbumObject, Album>()
-            .Map(dest => dest.ImageUrl, src => src.Images.Any() ? src.Images.First().Url : null);
+            // Try to get the last image, because it's the smallest one.
+            // We only show those images as album covers of tracks inside playlist view.
+            .Map(dest => dest.ImageUrl, src => src.Images.Any() ? src.Images.Last().Url : null);
 
         config.ForType<SimplifiedArtistObject, Artist>();
 
         config.ForType<DeviceObject, DeviceDto>()
             .Map(dest => dest.IsActive, src => src.Is_active)
             .Map(dest => dest.VolumePercent, src => src.Volume_percent);
+        private static string? GetPlaylistImage(ICollection<ImageObject> image)
+        {
+            // If there is more than one image, we want to use the second one, because it has similar
+            // dimensions to what we want to display to the user.
+            if (image.Count > 1) return image.ElementAt(index: 1).Url;
+
+            return image.FirstOrDefault()?.Url;
+        }
+
+        // Trims descriptions from anchor tags, because we can't render them correctly and their hrefs
+        // often contain internal Spotify links. After we Html decode remaining text.
+        private static string? TrimAndDecodePlaylistDescription(string? description)
+        {
+            if (string.IsNullOrEmpty(description)) return description;
+
+            var sentences = Regex.Split(description, @"(?<=[.!?])\s+");
+
+            return HttpUtility.HtmlDecode(
+                string.Join(" ", sentences.Where(s => !s.Contains("</a>")))
+            );
+        }
     }
-
-    // Trims descriptions from anchor tags, because we can't render them correctly and their hrefs
-    // often contain internal Spotify links. After we Html decode remaining text.
-    private static string? TrimAndDecodePlaylistDescription(string? description)
-    {
-        if (string.IsNullOrEmpty(description)) return description;
-
-        var sentences = Regex.Split(description, @"(?<=[.!?])\s+");
-
-        return HttpUtility.HtmlDecode(
-            string.Join(" ", sentences.Where(s => !s.Contains("</a>")))
-        );
-    }
-}
