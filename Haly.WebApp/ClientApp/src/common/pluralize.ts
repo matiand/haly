e6d379ba2bd8c55a006d7b9d52568d661c8340ae
matiand <15248621+matiand@users.noreply.github.com>
2023-07-04 17:23:03 +0200

    Add pluralize helper

---
+++ Haly.WebApp/ClientApp/src/common/pluralize.ts
@@ -0,0 +1,5 @@
+export function pluralize(noun: string, amount: number) {
+    const amountString = amount.toLocaleString();
+
+    return amount > 1 ? `${amountString} ${noun}s` : `${amountString} ${noun}`;
+}
