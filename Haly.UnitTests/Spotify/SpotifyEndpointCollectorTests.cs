ac472b10ef03c3c3c26be4094c5a1371731cad51
matiand <15248621+matiand@users.noreply.github.com>
2023-08-10 22:21:17 +0200

    Replace Moq with NSubstitute

--- Haly.UnitTests/Spotify/SpotifyEndpointCollectorTests.cs
+++ Haly.UnitTests/Spotify/SpotifyEndpointCollectorTests.cs
@@ -1,7 +1,7 @@
 using FluentAssertions;
 using Haly.GeneratedClients;
 using Haly.WebApp.ThirdPartyApis.Spotify;
-using Moq;
+using NSubstitute;
 
 namespace Haly.UnitTests.Spotify;
 
@@ -16,7 +16,7 @@ public class SpotifyEndpointCollectorTests
     {
         // Arrange
         var endpointCollector = new SpotifyEndpointCollector();
-        var endpointFnMock = new Mock<Func<int, int, Task<FakePagingObject>>>();
+        var endpointFnMock = Substitute.For<Func<int, int, Task<FakePagingObject>>>();
 
         var endpointTotal = total;
         var endpointLimit = limit;
@@ -24,30 +24,31 @@ public class SpotifyEndpointCollectorTests
         var endpointPaginations = CreateEndpointPaginations(endpointTotal, endpointLimit, endpointData);
 
         var expectedCalls = endpointPaginations.Count;
-        endpointFnMock.Setup(x => x(It.IsAny<int>(), It.IsAny<int>()))
-            .ReturnsAsync(() => endpointPaginations.Dequeue());
+        endpointFnMock(Arg.Any<int>(), Arg.Any<int>())
+            .Returns(_ => Task.FromResult(endpointPaginations.Dequeue()));
 
         // Act
         var data = await endpointCollector.FetchConcurrently(
-            endpointFn: endpointFnMock.Object,
+            endpointFn: endpointFnMock,
             dataFn: (data) => data.Items,
             endpointLimit: 50, maxConcurrentRequests: 2);
 
         // Assert
         data.Count.Should().Be(endpointTotal);
         data.Should().BeEquivalentTo(endpointData);
-        endpointFnMock.Verify(x => x(It.IsAny<int>(), It.IsAny<int>()), Times.Exactly(expectedCalls));
+        await endpointFnMock.Received(expectedCalls).Invoke(Arg.Any<int>(), Arg.Any<int>());
     }
 
     [Theory]
     [InlineData(986, 50, 490)]
     [InlineData(500, 50, 450)]
     [InlineData(98, 50, 47)]
-    public async Task FetchConcurrently_CollectsSpecifiedData_WhenStartingOffsetIsBiggerThan0(int total, int limit, int startingOffset)
+    public async Task FetchConcurrently_CollectsSpecifiedData_WhenStartingOffsetIsBiggerThan0(int total, int limit,
+        int startingOffset)
     {
         // Arrange
         var endpointCollector = new SpotifyEndpointCollector();
-        var endpointFnMock = new Mock<Func<int, int, Task<FakePagingObject>>>();
+        var endpointFnMock = Substitute.For<Func<int, int, Task<FakePagingObject>>>();
 
         var endpointTotal = total;
         var endpointLimit = limit;
@@ -55,27 +56,29 @@ public class SpotifyEndpointCollectorTests
         var endpointPaginations = CreateEndpointPaginations(endpointTotal, endpointLimit, endpointData, startingOffset);
 
         var expectedCalls = endpointPaginations.Count;
-        endpointFnMock.Setup(x => x(It.IsAny<int>(), It.IsAny<int>()))
-            .ReturnsAsync(() => endpointPaginations.Dequeue());
+        endpointFnMock(Arg.Any<int>(), Arg.Any<int>())
+            .Returns(_ => Task.FromResult(endpointPaginations.Dequeue()));
 
         // Act
         var data = await endpointCollector.FetchConcurrently(
-            endpointFn: endpointFnMock.Object,
+            endpointFn: endpointFnMock,
             dataFn: (data) => data.Items,
             endpointLimit: 50, maxConcurrentRequests: 4, startingOffset: startingOffset);
 
         // Assert
         data.Count.Should().Be(endpointTotal - startingOffset);
         data.Should().BeEquivalentTo(endpointData.Skip(startingOffset));
-        endpointFnMock.Verify(x => x(It.IsAny<int>(), It.IsAny<int>()), Times.Exactly(expectedCalls));
+        await endpointFnMock.Received(expectedCalls).Invoke(Arg.Any<int>(), Arg.Any<int>());
     }
 
+    // This has to be public, otherwise these tests fail.
     public class FakePagingObject : PagingObject
     {
         public List<int> Items { get; set; } = new();
     }
 
-    private static Queue<FakePagingObject> CreateEndpointPaginations(int endpointTotal, int endpointLimit, List<int> endpointData, int startingOffset = 0)
+    private static Queue<FakePagingObject> CreateEndpointPaginations(int endpointTotal, int endpointLimit,
+        List<int> endpointData, int startingOffset = 0)
     {
         var remaining = endpointTotal - startingOffset;
         var endpointDataQueue = new Queue<FakePagingObject>();
