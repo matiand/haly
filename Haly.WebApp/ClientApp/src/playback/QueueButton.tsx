bfb422cc802f7acd17ce69207f3811ae87c602e7
matiand <15248621+matiand@users.noreply.github.com>
2023-09-07 12:35:39 +0200

    Fix QueueButton wrong navigation when app was just loaded

--- Haly.WebApp/ClientApp/src/playback/QueueButton.tsx
+++ Haly.WebApp/ClientApp/src/playback/QueueButton.tsx
@@ -3,15 +3,23 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import PlaybackButton from "./PlaybackButton";
 
+// Contents of window.history.state. Tested on Chrome, Firefox, Edge.
+type HistoryState = {
+    idx: number;
+};
+
 function QueueButton() {
     const navigate = useNavigate();
     const { pathname } = useLocation();
 
     const isInQueue = pathname.startsWith("/queue");
+    const historyState = window.history.state as HistoryState;
 
     const onClick = () => {
-        if (isInQueue) {
+        if (isInQueue && historyState.idx > 0) {
             navigate(-1);
+        } else if (isInQueue && historyState.idx === 0) {
+            navigate("/");
         } else {
             navigate("/queue");
         }
