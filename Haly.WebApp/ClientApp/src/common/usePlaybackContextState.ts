0b796c9b05e8e843e4b40046772c75251d78e305
matiand <15248621+matiand@users.noreply.github.com>
2023-09-25 13:12:46 +0200

    Optimize usePlaybackContextState hook

--- Haly.WebApp/ClientApp/src/common/usePlaybackContextState.ts
+++ Haly.WebApp/ClientApp/src/common/usePlaybackContextState.ts
@@ -1,23 +1,21 @@
 import { useAtomValue } from "jotai";
 import { useCallback } from "react";
 
-import { isTrackPausedAtom, playbackContextIdAtom, streamedTrackIdAtom } from "./atoms";
+import { isTrackPausedAtom, playbackContextIdAtom } from "./atoms";
 
 export type PlaybackContextState = "playing" | "paused" | "none";
 
 function usePlaybackContextState() {
     const playbackContextId = useAtomValue(playbackContextIdAtom);
-    const streamedTrackId = useAtomValue(streamedTrackIdAtom);
     const isPaused = useAtomValue(isTrackPausedAtom);
 
     return useCallback(
         (id: string) => {
             if (playbackContextId !== id) return "none";
-            if (!streamedTrackId) return "none";
 
             return isPaused ? "paused" : "playing";
         },
-        [isPaused, playbackContextId, streamedTrackId],
+        [isPaused, playbackContextId],
     );
 }
 
