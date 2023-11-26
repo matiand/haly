26a23908e43f1da5367b5be0a72f8d4e4f93d800
matiand <15248621+matiand@users.noreply.github.com>
2023-11-26 23:41:02 +0100

    Make ForcePlaylistSync menu item work for 'Liked Songs'

--- Haly.WebApp/ClientApp/src/menus/items/ForcePlaylistSyncMenuItem.tsx
+++ Haly.WebApp/ClientApp/src/menus/items/ForcePlaylistSyncMenuItem.tsx
@@ -1,7 +1,9 @@
 import { MenuItem } from "@szhsin/react-menu";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSetAtom } from "jotai";
 import toast from "react-hot-toast";
 
+import { isLikedSongsCollectionChangedAtom } from "../../common/atoms/trackAtoms";
 import { GetPlaylistQueryKey } from "../../common/queryKeys";
 import halyClient from "../../halyClient";
 
@@ -10,16 +12,36 @@ type ForcePlaylistSyncMenuItemProps = {
     isLikedSongsCollection?: boolean;
 };
 
-function ForcePlaylistSyncMenuItem({ id, isLikedSongsCollection }: ForcePlaylistSyncMenuItemProps) {
+function ForcePlaylistSyncMenuItem({ id, isLikedSongsCollection = false }: ForcePlaylistSyncMenuItemProps) {
+    const setIsLikedSongsCollectionChanged = useSetAtom(isLikedSongsCollectionChangedAtom);
     const queryClient = useQueryClient();
-    const forcePlaylistSync = useMutation(() => halyClient.playlists.putPlaylist({ id, forceUpdate: true }), {
-        onSuccess: () => {
-            queryClient.invalidateQueries(GetPlaylistQueryKey(id));
-            toast("Playlist synced.");
-        },
-    });
 
-    return <MenuItem onClick={() => forcePlaylistSync.mutate()}>Force playlist sync</MenuItem>;
+    const forcePlaylistSync = useMutation(
+        async (isLikedSongs: boolean) => {
+            if (isLikedSongs) {
+                setIsLikedSongsCollectionChanged(true);
+            } else {
+                await halyClient.playlists.putPlaylist({
+                    id,
+                    forceUpdate: true,
+                });
+            }
+
+            return isLikedSongs;
+        },
+        {
+            onSuccess: (isLikedSongs) => {
+                // Liked songs are managed by useLikedSongsManagement hook.
+                if (!isLikedSongs) {
+                    queryClient.invalidateQueries(GetPlaylistQueryKey(id));
+                }
+
+                toast("Playlist synced.");
+            },
+        },
+    );
+
+    return <MenuItem onClick={() => forcePlaylistSync.mutate(isLikedSongsCollection)}>Force playlist sync</MenuItem>;
 }
 
 export default ForcePlaylistSyncMenuItem;
