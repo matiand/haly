9f860f2fc949023ceec0cda5a028cfd12be21a75
matiand <15248621+matiand@users.noreply.github.com>
2024-03-19 14:41:06 +0100

    Fix the VisuallyHidden component to actually hide its content

--- Haly.WebApp/ClientApp/src/ui/VisuallyHidden.tsx
+++ Haly.WebApp/ClientApp/src/ui/VisuallyHidden.tsx
@@ -1,9 +1,12 @@
 import { styled } from "../common/theme";
 
 const VisuallyHidden = styled("span", {
+    border: 0,
     clip: "rect(0 0 0 0)",
     clipPath: "inset(50%)",
     height: "1px",
+    margin: "-1px",
+    padding: 0,
     overflow: "hidden",
     position: "absolute",
     whiteSpace: "nowrap",
